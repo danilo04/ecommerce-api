4a5b22281bd99d8f398e718b312a793d25a21c99
Danilo Domínguez Pérez <danilo.dominguez@automattic.com>
2024-12-16 08:43:52 -0600

    Fix eslint setup and warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { 
+    globals: {
+      ...globals.browser, // Keep existing browser globals
+      process: 'readonly', // Add 'process' as a global variable
+    },
+  }},
   pluginJs.configs.recommended,
 ];
